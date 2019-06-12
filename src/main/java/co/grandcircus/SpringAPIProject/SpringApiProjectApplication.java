87dffe64b66a1df2a2e3df8c4a43f7d3b3466b5a
chan <cpark272@gmail.com>
2019-06-12 14:45:06 -0400

    initial commit

---
+++ src/main/java/co/grandcircus/SpringAPIProject/SpringApiProjectApplication.java
@@ -0,0 +1,13 @@
+package co.grandcircus.SpringAPIProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiProjectApplication.class, args);
+	}
+
+}
